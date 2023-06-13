593f96161f64791188f247e3854b4c96b346c547
Ruben Lopez Ruiz <24.lopezr@gmail.com>
2023-06-13 14:46:52 +0200

    Files for Cosmics MiniAOD production from AOD

--- MiniAOD_prod/crab_Cosmics_2022D_PromptReco_v1_AOD-MiniAOD.py
+++ MiniAOD_prod/crab_Cosmics_2022D_PromptReco_v1_AOD-MiniAOD.py
@@ -7,26 +7,26 @@ config = config()
 
 # General
 config.General.workArea = '/afs/cern.ch/user/r/rlopezru/private/DisplacedCollection_FW/CMSSW_13_2_0_pre1/src/DisplacedMuons-FrameWork/MiniAOD_prod/crab_projects/'
-config.General.requestName = 'Cosmics_2022G_PromptReco_CMSSW_13_2_0_pre1_MiniAOD'
+config.General.requestName = 'Cosmics_2022D_PromptReco_CMSSW_13_2_0_pre1_MiniAOD'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 config.General.instance = 'prod'
 
 # JobType
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'Cosmics_PromptReco_EFG_PAT.py'
+config.JobType.psetName = 'Cosmics_PromptReco_ABCD_PAT.py'
 config.JobType.maxMemoryMB = 3000
 config.JobType.allowUndistributedCMSSW = True
 config.JobType.numCores = 2
 
 # Data
-config.Data.inputDataset = '/NoBPTX/Run2022G-PromptReco-v1/AOD'
+config.Data.inputDataset = '/NoBPTX/Run2022D-PromptReco-v1/AOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1 # splitting to get ~500 output files
 config.Data.outLFNDirBase = '/store/user/rlopezru/Samples/' # modify accordingly
-config.Data.publication = False
-config.Data.outputDatasetTag = 'Cosmics_2022G_PromptReco_CMSSW_13_2_0_pre1_MiniAOD'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'Cosmics_2022D_PromptReco_CMSSW_13_2_0_pre1_MiniAOD'
 
 # Site
 config.Site.storageSite = 'T2_ES_IFCA'
