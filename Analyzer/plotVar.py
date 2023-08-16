933b7b1d826308bd06f5ff12ad353d42a1a143b6
Ruben Lopez Ruiz <24.lopezr@gmail.com>
2023-08-16 13:13:43 +0200

    Plotter class

--- Analyzer/plotVar.py
+++ Analyzer/plotVar.py
@@ -1,13 +1,24 @@
+import sys
 import os
 import ROOT as r
 import numpy as np
 import library
 import argparse
 from datetime import datetime
+from Plot import Plot
+
+# import library
+cutslib = library.cuts
+variableslib = library.variables
+sampleslib = library.samples
+legendlib = library.legendNames
+
+colors = {'dsa': r.kRed+2, 'dmu_dsa': r.kRed, 'dgl': r.kBlue+2, 'dmu_dgl': r.kBlue}
 
 # Input arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--muon',          required=False, type=str, dest='muon', default='dmu_dsa')
+parser.add_argument('--eff',           required=False, action='store_true', dest='efficiency')
+parser.add_argument('--muon',          required=False, type=str, dest='muon', nargs='*', default=['dmu_dsa'])
 parser.add_argument('--var',           required=False, type=str, dest='var', default='dz')
 parser.add_argument('--sample',        required=False, type=str, dest='sample', default='Cosmics2022C')
 parser.add_argument('--file',          required=False, type=str, dest='filename', default=None)
@@ -21,15 +32,18 @@ if __name__=='__main__':
     r.setTDRStyle()
     r.gROOT.SetBatch(1)
 
-    cutslib = library.cuts
-    variableslib = library.variables
-    sampleslib = library.samples
+    for muon in args.muon:
+        if muon not in ['dsa','dgl','dmu_dsa','dmu_dgl']: 
+            print("Error: incorrect muon type. Use one of these: dsa dgl dmu_dsa dmu_dgl")
+            sys.exit()
+    if args.var not in variableslib: 
+            print("Error: unsupported variable.")
+            sys.exit()
+    if args.sample not in sampleslib: 
+            print("Error: unsupported sample.")
+            sys.exit()
 
-    if args.muon not in ['dsa','dgl','dmu_dsa','dmu_dgl']: print("Error: incorrect muon type. Use one of these: dsa dgl dmu_dsa dmu_dgl")
-    if args.var not in variableslib: print("Error: unsupported variable.")
-    if args.sample not in sampleslib: print("Error: unsupported sample.")
-
-    mtype = args.muon
+    mtypes = args.muon
     outdir = args.outdir
     if not os.path.exists(outdir): os.makedirs(outdir)
 
@@ -41,65 +55,48 @@ if __name__=='__main__':
         _sample = sampleslib[args.sample]
         tChain = _sample.getTChain("Events")
 
-    # read varexp
-    var = variableslib[args.var]
-    varexp = var.getStr(mtype)
-
-    # buid selection string
-    weightParts = []
-    selectionParts = []
-    if args.cuts:
-        for cut in args.cuts:
-            _cut = cutslib[cut]
-            cutStr = _cut.getStr(mtype)
-            if _cut.perEvent: weightParts.append(f'({cutStr})')
-            else: selectionParts.append(f'({cutStr})')
-        weightStr = "&&".join(weightParts)
-        selectionStr = "&&".join(selectionParts)
-        selection = f"({weightStr})*({selectionStr})"
-
-    print(f"Args to tChain.Draw():")
-    print(f"    varexp:    {varexp}")
-    print(f"    selection: {selection}")
-
-    if var.vbins is not None: h = r.TH1F("h", ";"+var.title()+";N events", var.nbins, var.vbins)
-    else:                     h = r.TH1F("h", ";"+var.title()+";N events", var.nbins, var.xmin, var.xmax)
-    if args.maxEntries==-1:
-        tChain.Draw(varexp+">>h", selection, "goff")
-    else:
-        tChain.Draw(varexp+">>h", selection, "goff", args.maxEntries)
-
     if args.filename:
-        canvasName = "_".join(['var', "file", mtype, args.var, datetime.now().strftime("%d%m%Y_%H%M%S")])
+        canvasName = "_".join(['var', "file", '-'.join(mtypes), args.var, datetime.now().strftime("%d%m%Y_%H%M%S")])
     else:
-        canvasName = "_".join(['var', args.sample, mtype, args.var, datetime.now().strftime("%d%m%Y_%H%M%S")])
-    c = r.TCanvas(canvasName, canvasName)
-    c.SetFillStyle(4000)
-    c.cd()
-    c.GetPad(0).SetLogy(1)
-    h.Draw("HIST");
-    print(f"    n of entries:     {h.GetEntries()}")
-    latex = r.TLatex()
-    latex.SetNDC();
-    latex.SetTextAngle(0);
-    latex.SetTextColor(r.kBlack);
-    latex.SetTextFont(42);
-    latex.SetTextAlign(11);
-    latex.SetTextSize(0.04);
-    latex.DrawLatex(0.20, 0.93, "#bf{CMS} #it{Internal}")
-
-    latex = r.TLatex()
-    latex.SetNDC();
-    latex.SetTextAngle(0);
-    latex.SetTextColor(r.kBlack);
-    latex.SetTextFont(42);
-    latex.SetTextAlign(31);
-    latex.SetTextSize(0.03);
-    if not args.filename:
-        latex.DrawLatex(0.88, 0.93, _sample.name)
+        canvasName = "_".join(['var', args.sample, '-'.join(mtypes), args.var, datetime.now().strftime("%d%m%Y_%H%M%S")])
+    plot = Plot(canvasName, args.outdir)
+    if args.efficiency:
+        plot.initLegend()
     else:
-        latex.DrawLatex(0.88, 0.93, args.filename)
+        plot.initLegend(x0=.65, y0=.80, x1=.90, y1=.87)
 
-    c.SaveAs(f"{outdir}/{canvasName}.png")
-    c.SaveAs(f"{outdir}/{canvasName}.pdf")
-    c.SaveAs(f"{outdir}/{canvasName}.C")
+    for mtype in mtypes:
+        # read varexp
+        var = variableslib[args.var]
+        varexp = var.getStr(mtype)
+
+        # buid selection string
+        weightParts = []
+        selectionParts = []
+        if args.cuts:
+            for cut in args.cuts:
+                _cut = cutslib[cut]
+                cutStr = _cut.getStr(mtype)
+                if _cut.perEvent: weightParts.append(f'({cutStr})')
+                else: selectionParts.append(f'({cutStr})')
+            weightStr = "&&".join(weightParts)
+            selectionStr = "&&".join(selectionParts)
+            selection = f"({weightStr})*({selectionStr})"
+
+        print(f"----------------------------------------------")
+        print(f">> Drawing histogram:")
+        print(f"    varexp:              {varexp}")
+        print(f"    selection:           {selection}")
+
+        if var.vbins is not None: h = r.TH1F(f"h{mtype}", ";"+var.title()+";N events", var.nbins, var.vbins)
+        else:                     h = r.TH1F(f"h{mtype}", ";"+var.title()+";N events", var.nbins, var.xmin, var.xmax)
+        
+        if args.maxEntries==-1:
+            tChain.Draw(varexp+f">>h{mtype}", selection, "goff")
+        else:
+            tChain.Draw(varexp+f">>h{mtype}", selection, "goff", args.maxEntries)
+        print(f"    N of entries in hist: {h.GetEntries()}")
+        plot.addHist(h, legend=legendlib[mtype], color=colors[mtype])
+    
+    plot.makeVarPlot(ylog=True)
+    plot.save()
