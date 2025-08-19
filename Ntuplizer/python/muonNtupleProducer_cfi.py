6b2568a262903cea7d6ba668836531cdbd3b2877
fernanpe <pedro.fernandez.manteca@cern.ch>
2025-08-19 13:02:47 +0200

    missing file

--- Ntuplizer/python/muonNtupleProducer_cfi.py
+++ Ntuplizer/python/muonNtupleProducer_cfi.py
@@ -2,5 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 muonNtupleProducer = cms.EDAnalyzer("MuonNtupleProducer",
     muons = cms.InputTag("muons"),
-    cosmicMuons = cms.InputTag("cosmicMuons")
+    cosmicMuons = cms.InputTag("cosmicMuons"),
+    segmentsDt = cms.InputTag('dt4DSegments'),
+    segmentsCSC = cms.InputTag('cscSegments'),
 )
