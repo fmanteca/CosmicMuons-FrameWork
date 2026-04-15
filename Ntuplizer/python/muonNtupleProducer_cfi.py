de15626e2df495c7fa6a5128580a98adc63243c8
fernanpe <pedro.fernandez.manteca@cern.ch>
2026-04-15 16:21:29 +0200

    update

--- Ntuplizer/python/muonNtupleProducer_cfi.py
+++ Ntuplizer/python/muonNtupleProducer_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 muonNtupleProducer = cms.EDAnalyzer("MuonNtupleProducer",
     muons = cms.InputTag("muons1Leg"),
-    segmentsDt = cms.InputTag('dt4DSegments'),
-    segmentsCSC = cms.InputTag('cscSegments'),
+    # segmentsDt = cms.InputTag('dt4DSegments'),
+    # segmentsCSC = cms.InputTag('cscSegments'),
 )
