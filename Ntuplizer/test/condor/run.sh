b62a551d03e89ec4053d5d4facdf0f195ef4de4e
fernanpe <pedro.fernandez.manteca@cern.ch>
2026-04-16 11:28:19 +0200

    updated run.sh

--- Ntuplizer/test/condor/run.sh
+++ Ntuplizer/test/condor/run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+export X509_USER_PROXY=/afs/cern.ch/user/f/fernanpe/.x509up_96649
+voms-proxy-info
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
 
 cd /eos/user/f/fernanpe/CMSSW_15_0_5/src/CosmicMuons-FrameWork/Ntuplizer/test/condor
 cmsenv
